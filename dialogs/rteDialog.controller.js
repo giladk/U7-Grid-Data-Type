f65f18730dafb842f319aef201fda2cbf2eb71ed
Gerty Engrie <gerty.engrie@district01.be>
2019-08-20 08:45:17 +0200

    umbraco 8.1 changes

---
+++ dialogs/rteDialog.controller.js
@@ -0,0 +1,19 @@
+﻿angular.module("umbraco").controller("u7dtg.RteDialogController",
+    function ($scope, editorService) {
+        $scope.data = $scope.model.dialogData;
+        var vm = this;
+        vm.submit = submit;
+        vm.close = close;
+
+        function submit() {
+            if ($scope.model.submit) {
+                $scope.model.submit($scope.data);
+            }
+        }
+
+        function close() {
+            if ($scope.model.close) {
+                $scope.model.close();
+            }
+        }
+    });
